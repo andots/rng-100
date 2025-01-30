f4c81bfd499cc91b3f1d2fa873e6fb0cd5d9cd7f
andots <18536+andots@users.noreply.github.com>
2025-01-30 18:53:58 +0900

    fix: slim deps

--- apps/web/src/components/RandomNumber.tsx
+++ apps/web/src/components/RandomNumber.tsx
@@ -1,12 +1,21 @@
 import type { Component } from "solid-js";
 
 import { Button } from "@repo/ui/button";
+import { Switch, SwitchControl, SwitchLabel, SwitchThumb } from "@repo/ui/switch";
 
 const RandomNumber: Component = () => {
   return (
     <div>
       <div>
         <Button>Click me</Button>
+      </div>
+      <div class="mt-5">
+        <Switch class="flex items-center space-x-2">
+          <SwitchControl>
+            <SwitchThumb />
+          </SwitchControl>
+          <SwitchLabel>Airplane Mode</SwitchLabel>
+        </Switch>
       </div>
     </div>
   );
