01b16190d73a7cece799b1b32e11746b52a85f91
andots <18536+andots@users.noreply.github.com>
2025-01-30 20:55:07 +0900

    fix: remove unused content

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -4,7 +4,7 @@ import type { Config } from "tailwindcss";
 const config: Pick<Config, "content"> = {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "../packages/ui/src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "../packages/ui/src/**/*.{html,js,jsx,md,mdx,ts,tsx}",
   ],
   ...sharedConfig,
 };
