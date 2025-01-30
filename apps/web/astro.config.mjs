2b47a3be88f30121a7642a9d74ac621761c971f3
andots <18536+andots@users.noreply.github.com>
2025-01-30 16:30:48 +0900

    chore: use vite-tsconfig-paths

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,10 +1,15 @@
 import solidJs from "@astrojs/solid-js";
-
 import tailwind from "@astrojs/tailwind";
+
 // @ts-check
 import { defineConfig } from "astro/config";
+
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs(), tailwind({ applyBaseStyles: false })],
+  vite: {
+    plugins: [tsconfigPaths()],
+  },
 });
